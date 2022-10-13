f90f85a275f3e7aba16d3aca3761e9b7bd201693
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-10-14 04:27:17 +0800

    adding ph database

--- args.js
+++ args.js
@@ -10,3 +10,31 @@ if(!Date.parse(mydate) || mydate == 'undefined'){
 }
 
 console.log(mydate)
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name ? ', function (name) {
+  if(name == 'rogmer'){
+    rl.question('You are an admin enter your password: ', function (password) {
+        if(password === 'robudex'){
+            console.log('Welcome')
+        }else{
+            console.log('unauthorized Access!')
+        }
+        rl.close();
+      });
+  }else{
+    console.log('You are not the admin')
+    process.exit(0)
+  }
+  
+});
+
+rl.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
--- syncdb.js
+++ syncdb.js
@@ -1,6 +1,23 @@
 require('dotenv').config()
 const sequelize = require('sequelize')
 const { DataTypes } = require('sequelize')
+const mysql = require('mysql2/promise');
+
+//THIS SCRIPT EXPECT ARGUMENTS
+const args = process.argv.slice(2);
+const choosedate = args[0];
+const option  = args[1]
+
+if(choosedate == 'Undefined' ||  !Date.parse(choosedate)){
+    console.log('Date Arguments is need')
+    console.log("Must input valid date (yyyy-mm-dd') format")
+    process.exit(0)
+}
+if(option == 'Undefined' || option == '' || option !== 'check' || option !== 'startsync'){
+    console.log('No option provided..')
+    console.log('select between check|startsync')
+    process.exit(0)
+}
 // MYSQLSERVERHOST=localhost
 // MYSQLSERVERUSER=root
 // MYSQLSERVERPWD=sbtdbjapan@2022
@@ -10,14 +27,24 @@ const mysqlserver_user = process.env.MYSQLSERVERUSER
 const mysqlserver_pwd = process.env.MYSQLSERVERPWD
 const mysqlserver_db = process.env.MYSQLSERVERDB
 
+const mysqlserverph_host = process.env.MYSQLSERVERPHHOST
+const mysqlserverph_user = process.env.MYSQLSERVERPHUSER
+const mysqlserverph_pwd = process.env.MYSQLSERVERPHPWD
+const mysqlserverph_db = process.env.MYSQLSERVERPHDB
+
+
+
 const db = new sequelize(mysqlserver_db,mysqlserver_user,mysqlserver_pwd, {
     host: mysqlserver_host,
     dialect: 'mysql'
 })
 
+const countPhDBCdr = async () => {
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'test'});
+     return connection.execute(`SELECT Count(*) FROM WHERE getDate= ?, [${choosedate}]` );
+}
 
-
-connectDB = async() => {
+const connectBackupDB = async() => {
     try {
         await db.authenticate()
         console.log('Connected....')
@@ -26,7 +53,7 @@ connectDB = async() => {
     }
 }
 
-connectDB()
+connectBackupDB()
 
 const InboundCdr = db.define('InboundCdr', {
     date: {
@@ -81,22 +108,50 @@ const findAllCdr = async () => {
     }
 }
 
-const countAllCdr = async () => {
+const syncDb = async () => {
     try {
-        const {count, row} = await InboundCdr.findAndCountAll()
-        console.log(count)
-        if(count == 0){
-            console.log('No Found Records')
+        let {count, row} = await InboundCdr.findAndCountAll({where:{date:choosedate}})
+        let  countph = await countPhDBCdr()
+        count = parseInt(count)
+        countph = parseInt(countph)
+        if(countph > count){
+            console.log('Uploading backup cdr to the Main DB...')
+        }else if(count > countph){
+            console.log('Uploading backupd cdr to Ph Db...')
         }else{
-            const cdrs  = await findAllCdr()
-            for(let cdr of cdrs){
-                console.log(cdr.date)
-            }
+            console.log('Two Database are sync in records..No need to run sync')
         }
     }catch(error){
         console.log(error)
     }
 }
 
-countAllCdr()
+const countAllCdr = async () => {
+    try {
+        let {count, row} = await InboundCdr.findAndCountAll()
+        let countph = await countPhDBCdr()
+        count = parseInt(count)
+        countph = parseInt(countph)
+        if(countph > count){
+            console.log(`The Main DB have missing of ${countph}-${count} please start sync now`)
+        }else if(count > countph){
+            console.log(`The Ph DB have missing of ${count}-${countph} please start sync now`)
+            // for(let cdr of cdrs){
+            //     console.log(cdr.date)
+            // }
+        }else{
+            console.log('Two Database are sync in records..No need to run sync')
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+if(option == 'check'){
+    countAllCdr()
+}else if (option == 'startsync'){
+    syncDb() 
+}
+
 
