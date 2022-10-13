3ad13e2254a0bcfa6347112c6caad5e7e212a8a5
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-10-14 01:50:37 +0800

    adding nodejs version

--- syncdb.js
+++ syncdb.js
@@ -86,6 +86,7 @@ const findAllCdr = async () => {
 const countAllCdr = async () => {
     try {
         const totalcdrs = await InboundCdr.findAndCountAll()
+        console.log(totalcdrs)
         if(totalcdrs ==0){
             console.log('No Found Records')
         }else{
