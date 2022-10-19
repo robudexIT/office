9d5e7f955bd4e942c1b6e09aaec63516c3aa077a
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-10-19 21:50:07 +0800

    latest commit

--- syncdb.js
+++ syncdb.js
@@ -292,7 +292,7 @@ const countAllCdr = async () => {
          query = `SELECT Count(*) FROM tblSBTCallDetails_Incoming WHERE CdtStartDate=? AND CdtCalledParty='0452909485'`
          let maindbcountcdr2  = await maindb2(query)
                      
-        maindbcountcdr2 =   parseInt(maindbcountcdr2[0]['Count(*)']))
+        maindbcountcdr2 =   parseInt(maindbcountcdr2[0]['Count(*)'])
         console.log('backup ' + countbackupcdr)
         console.log('phdb ' + countphcdr)
         console.log('maindb ' +maindbcountcdr)
