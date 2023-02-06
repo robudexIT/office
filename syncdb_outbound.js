3041e238396787bc39b2bc339000be0f8889fa19
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2023-02-07 03:44:44 +0800

    adding syncdb_outbound.js

--- syncdb_outbound.js
+++ syncdb_outbound.js
@@ -93,7 +93,7 @@ const syncdb = async (query) => {
          
          //query maindb cdrs;
          let choose_date = choosedate.replaceAll("-","")
-         query = `SELECT * FROM tblSBTCallDetails_AddressBook WHERE  CdtCallingParty IN(${extensions}) AND CdtStartDate=${choose_date};`
+         query = `SELECT * FROM tblSBTCallDetails_AddressBook WHERE  CdtCallingParty IN(${extensions}) ;`
          let maindbtcdrs  = await maindb(query)
          maindbtcdrs = maindbtcdrs.recordset
          let maindbcount = maindbtcdrs.length
