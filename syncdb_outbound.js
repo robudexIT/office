87da3da99492c1580d7e92374402d97add0518c2
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2023-02-07 02:04:53 +0800

    adding syncdb_outbound.js

--- syncdb_outbound.js
+++ syncdb_outbound.js
@@ -12,7 +12,7 @@ const sqlserver = require('mssql')
 const args = process.argv.slice(2);
 const choosedate = args[0];
 const option  = args[1]
-const extensions = args[2]
+const extensions = args[2].split(" ")
 
 console.log(option)
 if(choosedate == 'Undefined' ||  !Date.parse(choosedate)){
