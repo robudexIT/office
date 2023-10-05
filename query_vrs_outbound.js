0156a18c74cb5f54b2affc6f999cb9bab3e96bc3
Rogmer Bulaclac <rogmer2022.bulaclac@gmail.com>
2023-10-06 04:23:01 +0800

    adding outbound_query

--- query_vrs_outbound.js
+++ query_vrs_outbound.js
@@ -75,7 +75,7 @@ const syncDb = async () => {
 
         //query maindb cdrs;
         let choose_date = choosedate.replaceAll("-","")
-        query = `SELECT * FROM tblSBTCallDetails_AddressBook WHERE  ((CdtCallingParty='6336' AND CdtStartDate=${choose_date};`
+        query = `SELECT * FROM tblSBTCallDetails_AddressBook WHERE  CdtCallingParty='6336' AND CdtStartDate=${choose_date};`
         let maindbtcdrs  = await maindb(query)
         maindbtcdrs = maindbtcdrs.recordset
         let maindbcount = maindbtcdrs.length
